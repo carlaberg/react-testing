e41af1982b7f0c84afa993d86949143c8ffcf065
Carl Åberg <carl@sould.se>
2019-11-13 16:36:26 +0100

    First test passing

---
+++ src/__tests__/example.test.js
@@ -0,0 +1,7 @@
+describe('I contiain a group of tests that are related to each other', () => {
+  it('works as expected', () => {
+    const value = 1;
+    expect(value).toEqual(1);
+  });
+  console.log('hello test bulle');
+});
